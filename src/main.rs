c778cd5ef6b06d5eb8afa1da25fc5e44c4c16c98
Kohei Suzuki <eagletmt@gmail.com>
2017-09-17 15:49:36 +0900

    Support scp-like URI

--- src/main.rs
+++ src/main.rs
@@ -73,19 +73,7 @@ fn main() {
 fn clone(matches: &clap::ArgMatches) -> Result<i32, Error> {
     let arg = matches.value_of("URL").unwrap();
     let uri = parse_git_url(arg)?;
-    let path = match std::env::home_dir() {
-        Some(mut pathbuf) => {
-            pathbuf.push(".ghq"); // TODO: Make customizable
-            pathbuf.push(uri.host_str().unwrap());
-            for c in std::path::PathBuf::from(uri.path()).components().skip(1) {
-                pathbuf.push(c.as_os_str());
-            }
-            pathbuf
-        }
-        None => {
-            return Err(Error::Custom("Cannot get HOME directory".to_owned()));
-        }
-    };
+    let path = destination_path_for(&uri)?;
     debug!("Clone {} to {}", uri, path.display());
     let mut child = std::process::Command::new("git")
         .arg("clone")
@@ -97,8 +85,80 @@ fn clone(matches: &clap::ArgMatches) -> Result<i32, Error> {
 }
 
 fn parse_git_url(u: &str) -> Result<url::Url, Error> {
-    // TODO: Support [user@]host.xz:path/to/repo.git form
     // https://git-scm.com/docs/git-push#_git_urls_a_id_urls_a
-    let url = url::Url::parse("https://github.com").unwrap().join(u)?;
-    Ok(url)
+    match url::Url::parse(u) {
+        Ok(uri) => {
+            debug!("{} is absolute URI", u);
+            Ok(uri)
+        }
+        Err(url::ParseError::RelativeUrlWithoutBase) => {
+            // Try scp-like URL
+            if let Some(colon_idx) = u.find(':') {
+                match u.find('/') {
+                    Some(slash_idx) => {
+                        if slash_idx > colon_idx {
+                            return parse_scp_like_url(u, colon_idx);
+                        }
+                    }
+                    None => {
+                        return parse_scp_like_url(u, colon_idx);
+                    }
+                }
+            }
+            debug!("{} is GitHub.com URI", u);
+            // Map :user/:repo to https://github.com/:user/:repo
+            Ok(url::Url::parse("https://github.com").unwrap().join(u)?)
+        }
+        Err(e) => Err(Error::from(e)),
+    }
+}
+
+fn parse_scp_like_url(u: &str, colon_idx: usize) -> Result<url::Url, Error> {
+    debug!("{} is scp-like URI", u);
+    let user_and_host = &u[..colon_idx];
+    let path = &u[colon_idx + 1..];
+    Ok(url::Url::parse(
+        &format!("ssh://{}/{}", user_and_host, path),
+    )?)
+}
+
+fn destination_path_for(uri: &url::Url) -> Result<std::path::PathBuf, Error> {
+    match std::env::home_dir() {
+        Some(mut pathbuf) => {
+            pathbuf.push(".ghq"); // TODO: Make customizable
+            pathbuf.push(uri.host_str().unwrap());
+            for c in std::path::PathBuf::from(uri.path()).components().skip(1) {
+                pathbuf.push(c.as_os_str());
+            }
+            Ok(pathbuf)
+        }
+        None => Err(Error::Custom("Cannot get HOME directory".to_owned())),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn parse_https_url() {
+        assert_eq!(
+            super::url::Url::parse("https://github.com/eagletmt/clg").unwrap(),
+            super::parse_git_url("https://github.com/eagletmt/clg").unwrap()
+        );
+    }
+
+    #[test]
+    fn parse_ssh_url() {
+        assert_eq!(
+            super::url::Url::parse("ssh://git@github.com/eagletmt/clg").unwrap(),
+            super::parse_git_url("ssh://git@github.com/eagletmt/clg").unwrap()
+        );
+    }
+
+    #[test]
+    fn parse_scp_like_ssh_url() {
+        assert_eq!(
+            super::url::Url::parse("ssh://git@github.com/eagletmt/clg").unwrap(),
+            super::parse_git_url("git@github.com:eagletmt/clg").unwrap()
+        );
+    }
 }
